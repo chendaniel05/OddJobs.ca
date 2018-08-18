98063e6168cd3ffb02cc5d2521d08590c8df3403
unknown <celi.ho@gmail.com>
2018-08-17 23:04:30 -0400

    seeds update

---
+++ models/seeds.sql
@@ -0,0 +1,184 @@
+USE jobs;
+
+INSERT INTO Postings (title, description, employer, location, salary, availability, createdAt, updatedAt) 
+VALUES 
+('Dog Walker', 
+'Pick up and drop off dogs from their homes
+Ensure security of their homes when picking up and dropping off
+Understand special requests and conditions for each dog
+Walk dogs outdoors in all weather conditions (anywhere from 15 minutes to 2 hours at a time)
+Transport dogs safely to and from their homes in a company van or your own
+Give each dog love and attention
+Ensure each dog is getting exercise and potty breaks
+Ensure safety of the dogs you are walking and those (humans and dogs) around them
+Remove pet waste
+Maintain leadership of the pack of dogs (if walking multiple dogs at a time)
+Take photos of the dogs (so human clients can see how they are doing)
+Refresh water, wipe paws and give treats
+Communicate with clients and schedule appointments (may conduct consultations for new clients)
+Conduct initial consultation with new clients and fill out client information sheets
+Update clients with email including photos and videos of their dogs
+Answer inquiries related to your service, clients’ dogs
+Record which dog(s) you walked, how long the walks were, anything abnormal that you noticed during the walk
+Manage your own schedule via phone, email and text message
+Keep track of your time and submit to the employer
+
+Call (416)777-2766', 
+'Jordan James', 
+'Toronto, Ontario', 
+"$15 per hour", 
+"On-Call", 
+CURRENT_TIMESTAMP, 
+CURRENT_TIMESTAMP),
+('Household Keeper', 
+'Housekeeper Responsibilities
+Include:
+
+Performing a variety of cleaning activities such as sweeping, mopping, dusting and polishing
+Ensuring all rooms are cared for and inspected according to standards
+Protecting equipment and making sure there are no inadequacies
+Job brief
+We are looking for a professional Housekeeper able of attending to our facilities with integrity and attention to detail.
+
+The goal is to create a clean and orderly environment for our guests that will become a critical factor in maintaining and strengthening our reputation.
+
+Responsibilities
+Perform a variety of cleaning activities such as sweeping, mopping, dusting and polishing
+Ensure all rooms are cared for and inspected according to standards
+Protect equipment and make sure there are no inadequacies
+Notify superiors on any damages, deficits and disturbances
+Deal with reasonable complaints/requests with professionalism and patience
+Check stocking levels of all consumables and replace when appropriate
+Adhere strictly to rules regarding health and safety and be aware of any company-related practices
+Requirements
+Proven experience as a cleaner or housekeeper
+Ability to work with little supervision and maintain a high level of performance
+Customer-oriented and friendly
+Prioritization and time management skills
+Working quickly without compromising quality
+Knowledge of English language
+High school degree
+
+Give me a call @6475038543',
+'Christy Mohammed',
+'Hamilton, Ontario', 
+"$70 per house", 
+"On-Call", 
+CURRENT_TIMESTAMP, 
+CURRENT_TIMESTAMP),
+('Caregiver', 
+'Caregiver job summary
+Our established healthcare facility is in search of an experienced and compassionate Caregiver who understands the importance of meeting the needs of clients while helping them maintain a high quality of life. We are looking for a committed individual who has impeccable communication skills and is able to problem solve on the spot. Our Caregivers have a reputation for being cheerful and receptive as they administer care to our clients, and we are seeking someone who would be a great fit with our team.
+
+Caregiver skills
+High school diploma required (college degree preferred)
+Current and valid caregiver certification
+Current and valid first aid and CPR certification
+Extensive experience working with the elderly
+Impeccable written and oral communication skills
+Experience and knowledge working with patients who have chronic and acute conditions
+Ability to problem solve quickly
+Nursing background an asset
+
+Call: 9052312565', 
+'iCare Inc.', 
+'Vaughan, Ontario', 
+"$85 per day", 
+"On-Call", 
+CURRENT_TIMESTAMP, 
+CURRENT_TIMESTAMP),
+('Make-up Artist', 
+'Primary responsibilities
+Responsible for applying makeup and prosthetics to aesthetically enhance celebrities, performers, individuals, entertainers, or for special events such as weddings or dates. Work independently or with a team. May also style hair.
+Apply makeup to clients using a color palette and different tools such as mascara, sponges, eyebrow shapers, lip liners, brushes, and applicators.
+Work with clients to create various looks.
+Work on theatrical productions, movie sets, photography shoots, backstage at concerts, or on television shows.
+Apply makeup for big events including weddings, funerals, holidays, and outings.
+Style hair according to makeup.
+Set makeup with hairspray or other setting material.
+Apply touchups as necessary.
+Remove makeup from clothes if some is transferred.
+Use photographs or models for reference when consulting with clients on ideal look.
+Apply prosthetics on movie sets.
+Read scripts to ascertain the materials and the look required.
+Sketch designs for future looks.
+Purchase cosmetics and applicators from vendors and negotiate prices.
+Ensure makeup looks natural and well-applied under different lighting.
+Match skin color to color palettes to determine best looks.
+Teach makeup application to clients.
+Suggest skin care routines.
+Fit wigs and hairpieces.
+Work quickly and accurately under pressure.
+Wipe off makeup and reapply if client is unsatisfied.
+Maintain awareness of health and safety issues and legislation.
+
+Email us at meganpromakeup@mail.com.', 
+'Megan Professional Make-up Inc.', 
+'Montreal, Quebec', 
+"$150 per session", 
+"On-Call", 
+CURRENT_TIMESTAMP, 
+CURRENT_TIMESTAMP),
+('Moving company', 
+'Downtown Moving Company looking to hire awesome Drivers and helpers. We will provide training. Duties include: - Being able to operate a 16 ft. cube van in Toronto traffic - Professional and positive attitude. We offer a flexible schedule -- (we have work all month long -- however the action is at the end of the month. This job is perfect for independent contractors, artists, or people who like to be able work when they want. Job pays well, on top of that, we usually get great tips! Please send a resume
+
+
+Job Type: Contract
+
+Salary: $15.00 to $20.00 /hour
+
+Licence:
+
+Drivers License G (Required)
+
+Languages
+English
+
+Education
+Secondary (high) school graduation certificate
+
+Experience
+1 year to less than 2 years
+
+Specific Skills
+Load, unload and move products and materials by hand or with basic material handling equipment
+Weight Handling
+Up to 45 kg (100 lbs)
+Work Setting
+Moving and storage company
+Security and Safety
+Bondable; Criminal record check
+Own Tools/Equipment
+Steel-toed safety boots', 
+'Quick Move @ 4161230987', 
+'Toronto, Ontario', 
+"$120 per house", 
+"On-Call", 
+CURRENT_TIMESTAMP, 
+CURRENT_TIMESTAMP),
+('Tutor', 
+'Tutor job summary
+Our after-school educational program seeks talented, experienced Tutors in all subject areas for students between grades 4 and 12. We help both struggling and gifted students reach their full potential by supplementing the instruction they receive in class and guiding them toward study practices and aides that can help them excel. Tutors work with individual students one-on-one, though we occasionally organize group activities for students who are studying the same material. Our team offers compassionate, motivating assistance to students, and we expect our new hires to do the same. If you have experience tutoring children, please submit an application.
+Tutor responsibilities
+Meet students at the school and go over their homework assignments in the applicable subject area
+Ask students about specific topics or subject areas that are giving them trouble or that warrant further exploration
+Review recent homework assignment and test scores, and work through problems or questions that the student did not answer correctly
+Employ proven study aides to prepare for upcoming tests and quizzes
+Take notes during tutoring sessions so you can follow up with the student later
+Communicate with parents/guardians about trouble areas or any other issue that requires attention
+
+Tutor skills
+Associate degree required
+Teaching certificate preferred
+3+ years’ experience tutoring children and teenagers
+Ability to pass a background check and drug screening
+Reliable transportation a must
+Able to develop positive rapport with students
+Excellent communication skills
+Established proficiency in subject matte', 
+'Quick Learn Tutor School', 
+'Toronto, Ontario', 
+"$25 per hour", 
+"On-Call", 
+CURRENT_TIMESTAMP, 
+CURRENT_TIMESTAMP)
